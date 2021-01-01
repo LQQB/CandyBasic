9a1520e930d648c046e9310c0ac5049ef6fac129
木子广大木木三 <897680736@qq.com>
2021-01-01 16:49:33 +0800

    feat: 项目初始化

---
+++ src/main/java/com/candy/basic/CandyBasicApplication.java
@@ -0,0 +1,13 @@
+package com.candy.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandyBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandyBasicApplication.class, args);
+	}
+
+}
